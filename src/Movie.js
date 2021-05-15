9ac32df465f79c6dc4ac90a4920bef059bff34bb
zad Babaei <zadbabaei@gmail.com>
2021-05-15 17:17:12 -0230

    testing styled components

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,28 @@
+import styled from"styled-components";
+
+
+
+
 let imgBaseUrl="https://image.tmdb.org/t/p/w500"
+
+const ImageText = styled.p`
+color: palevioletred;
+margin:10px;
+`;
+const Images = styled.img`
+margin:10px;
+`;
+
 
 function Movie(props){
     
     return( <div>
-            <img src={imgBaseUrl+props.movie.backdrop_path} alt={props.movie.title}></img>
-            <p>Movie title : {props.movie.title}</p>
-            <p>Movie ID : {props.movie.id}</p>
-            <p>Release_date  : {props.movie.release_date}</p>
+
+            <Images src={imgBaseUrl+props.movie.backdrop_path} alt={props.movie.title}></Images>
+            <ImageText>Movie title : {props.movie.title}</ImageText>
+            <ImageText>Movie ID : {props.movie.id}</ImageText>
+            <ImageText>Release_date  : {props.movie.release_date}</ImageText>
+
             </div>
         )
 }
