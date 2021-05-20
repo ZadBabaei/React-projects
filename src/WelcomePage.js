09f75edbb8b5ac438e4fdee2b07f6c7da98b036c
zad Babaei <zadbabaei@gmail.com>
2021-05-20 15:39:59 -0230

    working on welcome page (css)

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import movieTape from "./Images/movie tape.svg";
+// import { Link } from 'react-router-dom';
+import movieTape from "./Images/movie tape.png";
+import film from "./Images/film.svg";
 
 function WelcomePage() {
-  const Navigation = styled.nav`
-    display: flex;
-    justify-content: space-around;
-  `;
+  
+
+const MovieTapeImg = styled.img`
+margin:0;
+padding:0;
+background:transparent;
+
+width:90rem;
+`;
+const Film = styled.img`
+  margin: 0;
+  padding: 0;
+  width:85%;
+
+
+`;
+const Div2 = styled.div`
+display:flex;
+background: linear-gradient(180deg, rgba(52,68,88,1) 0%, rgba(0,0,0,1) 100%, rgba(0,212,255,1) 100%);
+margin:0;
+padding:0;
+
+
+`;
+// const TextTop = styled.h1`
+  
+//   color:red;
+//   font-size:120px;
+//   position:relative;
+//   top:400px;
+//   left:1600px;
+// `;
 
   return (
-    <div>
-      <img src={movieTape} alt="test" />
-      <Navigation>
-        <h1>welcome</h1>
-        <Link to="/SelectMovies">
-          <h1>select movies</h1>
-        </Link>
-      </Navigation>
+    <Div2>
 
-    </div>
+        <MovieTapeImg src={movieTape} alt={"movie image"}></MovieTapeImg>
+        <Film src={film} alt={"movie image"}></Film>
+
+
+
+
+    </Div2>
   );
 }
 
