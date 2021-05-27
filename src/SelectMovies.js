d6e756717c6ad831fc4ebe9f8be9863786fc09b0
zad Babaei <zadbabaei@gmail.com>
2021-05-26 22:38:21 -0230

    starting of select movies page

--- src/SelectMovies.js
+++ src/SelectMovies.js
@@ -1,32 +1,42 @@
 import './App.css';
 import 'antd/dist/antd.css';
 import Button from '@material-ui/core/Button';
-import Hero from './Hero';
-import MovieAutoComplete from './MovieAutoComplete';
-import { getMovieDataById } from './Search';
-import Movie from './Movie';
+import styled from 'styled-components';
 import { useState } from 'react';
+import transparent_films from './Images/transparent_films.svg';
 
 function SelectMovies() {
+  const ImageWraper = styled.div`
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    width: 50rem;
+    top: 0rem;
+    left: 0rem;
+  `;
+
+  const ButFimlImg = styled.img`
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    width: 50rem;
+    top: 0rem;
+    left: 0rem;
+  `;
+
+  //   const Film = styled.img`
+  //     margin: 0;
+  //     padding: 0;
+  //     position: absolute;
+  //     top: 23rem;
+  //     left: 12rem;
+  //   `;
   const [selectedMovies, setSelectedMovies] = useState([]);
   return (
     <div>
-      <Hero />
-      <div>
-        <MovieAutoComplete
-          onSelect={id => {
-            console.log('selected:', id);
-            getMovieDataById(id, movieData => {
-              console.log("here's the movie data:", movieData);
-              const newSelectedMovies = selectedMovies.concat([movieData]);
-              setSelectedMovies(newSelectedMovies);
-            });
-          }}
-        />
-      </div>
-      {selectedMovies.map(s => {
-        return <Movie movie={s} key={s.id} />;
-      })}
+      <ImageWraper>
+        <ButFimlImg src={transparent_films} alt="ds" />
+      </ImageWraper>
       <div>
         <Button variant="contained" color="primary">
           {' '}
